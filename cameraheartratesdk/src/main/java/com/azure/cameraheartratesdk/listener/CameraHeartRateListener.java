ea5dacf3d43bc24b15b39200e1ea9ee5d42cf348
HuangMinglun <873930354@qq.com>
2025-02-25 10:53:51 +0800

    增加注释

--- cameraheartratesdk/src/main/java/com/azure/cameraheartratesdk/listener/CameraHeartRateListener.java
+++ cameraheartratesdk/src/main/java/com/azure/cameraheartratesdk/listener/CameraHeartRateListener.java
@@ -1,9 +1,24 @@
 package com.azure.cameraheartratesdk.listener;
 
 public interface CameraHeartRateListener {
+    /**
+     * onHeartRate回调返回值为心率，单位为 次数/分钟
+     */
     void onHeartRate(int heartRate);
+    /**
+     * onSDNN回调返回值为sdnn
+    */
     void onSDNN(int sdnn);
+    /**
+     * onRMSSD回调返回值为rmssd
+     * */
     void onRMSSD(int rmssd);
+    /**
+     * 返回值为手指是否放置的状态，isDetected为true表示放置，false为未放置
+     * */
     void onFingerDetected(boolean isDetected);
+    /**
+     * 分别为心率\hrv有效值比例
+     * */
     void onEffectiveValueRate(float hrEffectiveValueRate,float hrvEffectiveValueRate);
 }
